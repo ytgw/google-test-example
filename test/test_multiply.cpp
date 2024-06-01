4b89813781d98e10f12c694652b7f81fc63620ce
ytgw <tagawa0920@gmail.com>
2024-06-01 05:41:53 +0000

    スタブ関数が複数回呼ばれる時に対応する。

--- test/test_multiply.cpp
+++ test/test_multiply.cpp
@@ -1,12 +1,23 @@
+#include <stdio.h>
 #include <gtest/gtest.h>
 #include "multiply.hpp"
+#include "print_int.hpp"
 
 
-void print_int(const int val) {
-    EXPECT_EQ(6, val);
-    std::cout << "stub function: " << val << std::endl;
+// TODO: 非同期で動くときなどの排他制御の実装
+static int expected_input = 0;
+
+void print_int(const int actual_input) {
+    EXPECT_EQ(actual_input, expected_input);
+    printf("stub of print_int: (actual arg, expected arg) = (%d, %d)\n", actual_input, expected_input);
 }
 
-TEST(Mock, Multiply) {
+TEST(Mock, Multiply1) {
+    expected_input = 6;
     print_multiply(2, 3);
+}
+
+TEST(Mock, Multiply2) {
+    expected_input = 12;
+    print_multiply(4, 3);
 }
